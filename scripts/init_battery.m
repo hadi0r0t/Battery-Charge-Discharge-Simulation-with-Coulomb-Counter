9581dff8ba2e3e9ffa90365c38354b8026694532
Hadi Rafat Talab <78220594+hadi0r0t@users.noreply.github.com>
2025-10-03 09:08:16 +0330

    Add files via upload

---
+++ scripts/init_battery.m
@@ -0,0 +1,31 @@
+%% run('init_battery.m')
+%% Battery Parameters
+Q_C    = 9000;       % Battery capacity [Coulomb] ~ 2.5 Ah
+R0    = 0.03;       % Ohmic resistance [Ohm]
+R1    = 0.02;      % RC branch 1 resistance [Ohm]
+C1   = 2000;       % RC branch 1 capacitance [F]
+R2    = 0.015;       % RC branch 2 resistance [Ohm]
+C2   = 10000;       % RC branch 2 capacitance [F]
+
+% Open circuit voltage (OCV) map
+SoC_vec = linspace(0,1,11);             % SOC grid
+OCV_vec = [3.0 3.4 3.6 3.7 3.8 3.9 ...
+           4.0 4.05 4.1 4.15 4.2];     % Example OCV curve [V]
+
+% Charge and discharge
+eta_charge = 0.995;
+eta_discharge = 1.000;
+
+
+% Controller parameters
+Icc          = 1.0;       % CC charge current [A]
+Idis        = -1.0;      % CC discharge current [A]
+Vmax        = 4.2;       % Charge cutoff voltage [V]
+Vcutoff      = 3.0;       % Discharge cutoff voltage [V]
+Iterm        = 0.05;      % Termination current for CV [A]
+T_cv_hold   = 300;       % Time hold in CV [s]
+T_rest_charge= 200;       % Rest time after charge [s]
+T_rest_dis  = 200;       % Rest time after discharge [s]
+
+Ts         = 0.05;      % Simulation step size [s]
+SoC0      = 0.8;       % Initial SoC
